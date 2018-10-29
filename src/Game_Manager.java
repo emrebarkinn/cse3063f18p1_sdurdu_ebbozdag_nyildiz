c6fe1d90b74803c831c7d2f22553f595f6590126
emrebarkinn <emrebarkinn@gmail.com>
2018-10-29 17:19:20 +0300

    Initial classes

---
+++ src/Game_Manager.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Game_Manager {
+    ArrayList<Player> players;
+    Player winner;
+    int iteration;
+    boolean inJail;
+    //Board=board;
+    //TODO create board class
+}
---
+++ src/Player.java
@@ -0,0 +1,9 @@
+public class Player {
+    int id;
+    String name;
+    double Money;
+    int position;
+
+    // TODO die object need to add
+
+}
