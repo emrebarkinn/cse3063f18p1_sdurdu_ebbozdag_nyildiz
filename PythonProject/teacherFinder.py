6c8cd2f2dce254f27a8eb212709eee24a9d5d7fd
emrebarkinn <emrebarkinn@gmail.com>
2018-12-21 21:02:27 +0300

    Python Project Folder is created

---
+++ PythonProject/teacherFinder.py
@@ -0,0 +1,43 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+
+class TeacherFinder:
+    teachers = []
+
+    def __init__(self):
+        r = requests.get("https://eng.marmara.edu.tr/akademik/akademik-kadro/ogretim-uyeleri")
+
+        data = r.text
+
+        soup = BeautifulSoup(data, 'html.parser')
+
+        j = 0
+        for aaa in soup.find_all('td'):
+
+            deneme = ''.join(aaa.findAll(text=True))
+            deneme2 = deneme.split()
+
+            if (j != 0) and (j % 6 == 0) and (j <= 678):
+                tempName = ""
+                for i in range(1, len(deneme2)):
+                    tempName += deneme2[i]
+                    tempName += " "
+
+                    i += 1
+                tempName += deneme2[0]
+                if tempName != "":
+                    self.teachers.append(tempName.replace(",", ""))
+            j += 1
+
+    def get_teachers(self):
+
+        return self.teachers
+
+    def print_teachers(self):
+
+        for i in self.teachers:
+            print(i)
+        return
+
