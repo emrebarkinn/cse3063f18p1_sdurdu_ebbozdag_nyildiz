d70689c488afba56521dd762b491dc3a5e0a0196
nuri <nuriidyliz@gmail.com>
2018-12-21 23:06:25 +0300

    trim, count word is done *pdf reader is not working

--- PythonProject/Main.py
+++ PythonProject/Main.py
@@ -1,8 +1,34 @@
+import PyPDF2
+'''
+def trim(x):
+    for i,element in enumerate(x):
+        x[i] = element.replace(".", "")
+
+'''
+'''
 f = open("input.txt", "r")
 if f.mode == 'r':
     contents = f.read()
     contents = contents.lower()
     x = contents.split(" ")
+
+    trim(x)
+    print(x)
+'''
+
+f = open('kulup.pdf', 'rb')
+pdfReader = PyPDF2.PdfFileReader(f)
+print(pdfReader.getNumPages())
+pageObj = pdfReader.getPage(1)
+print(pageObj.extractText())
+f.close()
+'''
+if f.mode == 'rb':
+    contents = f.read()
+    contents = contents.lower()
+    x = contents.split(" ")
+
+    trim(x)
     print(x)
 
 f1 = open("stopWords.txt", "r")
@@ -29,8 +55,9 @@ countedWords = []
 for item in x:
     t = tuple(item)
     if t not in seen:
-        countedWords.append(item, x)
+        countedWords.append([item,x.count(item)])
         seen.add(t)
 i, j = [0, 0]
 
 print(countedWords)
+'''
