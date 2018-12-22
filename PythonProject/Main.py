9e001e8daa442558cf0293ffeb418c8b6bcc4d70
nuri <nuriidyliz@gmail.com>
2018-12-22 15:07:50 +0300

    trim, count and reading pdf is nearly done

--- PythonProject/Main.py
+++ PythonProject/Main.py
@@ -1,38 +1,51 @@
-from TeacherFinder import TeacherFinder
+import PyPDF2
+from tika import parser
 
-f = open("input.txt", "r")
-if f.mode == 'r':
-    contents = f.read()
-    contents = contents.lower()
-    x = contents.split(" ")
-    print(x)
+class Main:
 
-f1 = open("stopWords.txt", "r")
-if f1.mode == 'r':
-    contents1 = f1.read()
-    y = contents1.split(",")
-    print(y)
-i, j = [0, 0]
-while (i < len(x)):
-    while (j < len(y)):
-        if (y[j] == x[i]):
-            x.remove(x[i])
-            i -= 1
-            continue
-        j += 1
-    j = 0
-    i += 1
+    pdfFile = 'Project1.pdf'
 
-print(x)
-i = 0
+    def trim(x):
+        trimmedChar = ["\n", "."]
+        for i, element in enumerate(x):
+            for char in trimmedChar:
+                x[i] = x[i].replace(char, "")
 
-seen = set()
-countedWords = []
-for item in x:
-    t = tuple(item)
-    if t not in seen:
-        countedWords.append(item, x)
-        seen.add(t)
-i, j = [0, 0]
+    parsed = parser.from_file(pdfFile)
+    content = parsed["content"]
+    print(type(content))
+    content = content.lower()
+    wordList = content.split(" ")
+    trim(wordList)
+    print(wordList)
 
-print(countedWords)
+    f1 = open("stopWords.txt", "r")
+    if f1.mode == 'r':
+        contents1 = f1.read()
+        y = contents1.split(",")
+        print(y)
+    i, j = [0, 0]
+    while (i < len(wordList)):
+        while (j < len(y)):
+            if (y[j] == wordList[i]):
+                wordList.remove(wordList[i])
+                i -= 1
+                continue
+            j += 1
+        j = 0
+        i += 1
+
+    print(wordList)
+    i = 0
+
+    seen = set()
+    countedWords = []
+    for item in wordList:
+        t = tuple(item)
+        if t not in seen:
+            countedWords.append([item, wordList.count(item)])
+            seen.add(t)
+    i, j = [0, 0]
+
+    print(countedWords)
+
