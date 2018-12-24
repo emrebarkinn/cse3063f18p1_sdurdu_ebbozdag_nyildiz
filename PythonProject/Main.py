9fa2647b5cb1c829be41641907854da5d332f475
sevdadurdu <sevdadurdu@outlook.com>
2018-12-25 01:26:51 +0300

    fixed writing to csv file

--- PythonProject/Main.py
+++ PythonProject/Main.py
@@ -73,13 +73,15 @@ class Main:
         for i in countedWords:
             i.append(i[1] / sum)
         print(countedWords)
+
         self.files_word_array.append(countedWords)
         with open(teacher_name.replace(" ", "_") + "/" + file_number + 'tf_list.csv', mode='w') as tf_list:
-            headers = ['Word', 'TfValue']
+
 
             tf_writer = csv.writer(tf_list, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            tf_writer.writerow(headers)
 
+            headers = ['Word', 'TfValue']
+            tf_writer.writerow(headers)
 
             count = 0
             for elements in countedWords:
@@ -90,27 +92,22 @@ class Main:
                 count += 1
                 if count == 50:
                     break
-        '''with open('Ali_Fuat_Alkaya/'+file_number+'tf_list.csv') as csv_file:
+        with open('Ali_Fuat_Alkaya/'+file_number+'tf_list.csv') as csv_file:
             csv_reader = csv.reader(csv_file)
-            d={}
+            d = {}
             for row in csv_reader:
-                d[row[0]] = row[1:]
-            print(tf_list)'''
-        files = ["0tf_list.csv", "1tf_list.csv", "2tf_list.csv"]
-        with open("output.tsv", "w", newline='') as fw:
-            cw = csv.writer(fw, delimiter="\t")  # output is tab delimited
-            for filename in files:
-                with open(filename, 'r') as f:
-                    cr = csv.reader(f)
-                    # get title
-                    for column_name in (x.strip() for x in next(cr)):
-                        cw.writerow([filename, column_name])
+                try:
+                    if row[0] == 'Word':
+                        continue
+
+                    d.update({row[0]: row[1]})
+                except:
+                    pass
 
 def main():
     aaa = Main()
     for i in range(0, 3):
         aaa.calculate_tf_words("Ali Fuat Alkaya", str(i))
-
 
 if __name__ == "__main__":
     main()
